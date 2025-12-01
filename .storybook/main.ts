d3d98c7b0c04fb6e2ca9b78ceacef53c490b2820
contumasionus <ivanapolonov3@gmail.com>
2025-12-01 09:20:41 +0300

    библиотека компонентов

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,16 @@
-// eslint-disable-next-line unicorn/prefer-node-protocol
-import path from 'path';
 import type { StorybookConfig } from '@storybook/vue3-vite';
 
-// eslint-disable-next-line unicorn/prefer-module
-const requirePath = typeof require === 'undefined' ? import.meta : require;
-
-function getAbsolutePath(packageName: string) {
-  return path.dirname(requirePath.resolve(path.join(packageName, 'package.json'))).replace(/^file:\/\//, '');
-}
-
 const config: StorybookConfig = {
-  framework: { name: getAbsolutePath('@storybook/vue3-vite'), options: {} },
-  stories: ['../src/docs/*.mdx', '../src/**/*.stories.ts'],
-  addons: ['@storybook/addon-docs'],
-  core: { disableTelemetry: true, disableWhatsNewNotifications: true },
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs"
+  ],
+  "framework": "@storybook/vue3-vite"
 };
-
 export default config;
